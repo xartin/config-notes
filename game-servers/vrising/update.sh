ee7dd9b0455c832268b7d46cbb557526118b1bac
Michael Crawford <mcrawford@eliteitminds.com>
2023-05-22 06:27:41 -0500

    vrising update.sh

--- game-servers/vrising/update.sh
+++ game-servers/vrising/update.sh
@@ -1,1 +1,1 @@
-/home/game-servers/steamcmd/steamcmd.sh +force_install_dir "/home/game-servers/vrising" +login anonymous +app_update 1829350 validate +quit
+/game-servers/steamcmd/steamcmd.sh +force_install_dir "/game-servers/vrising" +login anonymous +app_update 1829350 validate +quit
