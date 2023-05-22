4a6fe14f277b60d569634a0fd4f534a9458c655d
Michael Crawford <mcrawford@eliteitminds.com>
2023-05-22 06:26:40 -0500

    vrising start.sh

--- game-servers/vrising/start.sh
+++ game-servers/vrising/start.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-s=/home/game-servers/vrising
-p=/home/game-servers/vrising/persistentdata
+s=/game-servers/vrising
+p=/game-servers/vrising/persistentdata
 x=vrising
 echo "Updating V-Rising Dedicated Server files..."
 echo " "
-/home/game-servers/steamcmd/steamcmd.sh +force_install_dir "/home/game-servers/vrising" +login anonymous +app_update 1829350 validate +quit
+/game-servers/steamcmd/steamcmd.sh +force_install_dir "/game-servers/vrising" +login anonymous +app_update 1829350 validate +quit
 echo " "
-if [ -f "$p/Settings/AmokServerGameSettings.json" ]; then
-        echo "$p/Settings/AmokServerGameSettings.json found. Copying Master Config file."
-        cp "$p/Settings/AmokServerGameSettings.json" "$s/VRisingServer_Data/StreamingAssets/Settings/ServerGameSettings.json" 2>&1
+if [ -f "$p/Settings/ServerGameSettings.json" ]; then
+        echo "$p/Settings/ServerGameSettings.json found. Copying Master Config file."
+        cp "$p/Settings/ServerGameSettings.json" "$s/VRisingServer_Data/StreamingAssets/Settings/ServerGameSettings.json" 2>&1
 fi
-if [ -f "$p/Settings/AmokServerHostSettings.json" ]; then
-        echo "$p/Settings/AmokServerHostSettings.json found. Copying Master Config file."
-        cp "$p/Settings/AmokServerHostSettings.json" "$s/VRisingServer_Data/StreamingAssets/Settings/ServerHostSettings.json" 2>&1
+if [ -f "$p/Settings/ServerHostSettings.json" ]; then
+        echo "$p/Settings/ServerHostSettings.json found. Copying Master Config file."
+        cp "$p/Settings/ServerHostSettings.json" "$s/VRisingServer_Data/StreamingAssets/Settings/ServerHostSettings.json" 2>&1
 fi
 echo "Starting V Rising Dedicated Server"
 echo " "
-screen -dmS $x xvfb-run --auto-servernum --server-args='-screen 0 1920x1080x24:32' wine /home/game-servers/vrising/VRisingServer.exe -persistentDataPath $p -log
+screen -dmS $x xvfb-run --auto-servernum --server-args='-screen 0 1920x1080x24:32' wine $s/VRisingServer.exe -persistentDataPath $p -log
